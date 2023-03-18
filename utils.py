d256846f347c2d415a588f91ebd8784ed1cb7408
Paulo Luiz <paulolsouza19@gmail.com>
2023-03-18 02:28:33 -0300

    Implemented function that reads the data file and loads the graph corresponding to the file

--- utils.py
+++ utils.py
@@ -1,7 +1,31 @@
 from weightedGrapgh import WeightedGraph
 import pandas as pd
 
-df = pd.read_csv('critical_path/TOY.csv', ",")
-print(df["Nome"].unique()[1])
+def read_file(file_name: str):
+    df = pd.read_csv(file_name, ",") 
+    df = df.fillna(value="-")
+    num_discipline = df.shape[0]
+    dependences = ""
+
+    g1 = WeightedGraph(num_discipline + 2)
+    for i in range(num_discipline):
+        if df['Dependências'][i] != '-':
+            dependences = df['Dependências'][i]
+            if ';' in dependences:
+                dependences = dependences.split(';')
+                for j in dependences:
+                    u = df.index[df['Código'] == j].tolist()
+                    v = i 
+                    g1.add_directed_edge(u[0], v, 1)
+                    g1.add_directed_edge(v, num_discipline + 1, 1)
+            else:
+                u = df.index[df['Código'] == dependences].tolist()
+                v = i
+                g1.add_directed_edge(u[0], v, 1)
+                g1.add_directed_edge(v, num_discipline + 1, 1)
+        else:
+            g1.add_directed_edge(num_discipline, i, 0)
+            g1.add_directed_edge(i, num_discipline + 1, 1)
 
 
+        
